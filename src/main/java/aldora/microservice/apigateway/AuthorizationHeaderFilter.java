9b5bc44e74d67d357d05afe13d08f485fe9f5085
ShakalakaB <yunjie989@gmail.com>
2021-06-21 00:59:27 +0800

    Validate JWT token

---
+++ src/main/java/aldora/microservice/apigateway/AuthorizationHeaderFilter.java
@@ -0,0 +1,81 @@
+package aldora.microservice.apigateway;
+
+import com.google.common.base.Strings;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+
+@Component
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+    @Autowired
+    Environment environment;
+
+    public AuthorizationHeaderFilter() {
+        super(Config.class);
+    }
+
+    public static class Config {
+//        Put configuration properties here
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+
+            if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                return onError(exchange, "No authorization header", HttpStatus.UNAUTHORIZED);
+            }
+
+            String authorizationHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+            String jwt = authorizationHeader.replace("Santa", "");
+
+            if (!isJwtValid(jwt)) {
+                return onError(exchange, "JWT token is not valid", HttpStatus.UNAUTHORIZED);
+            }
+
+            return chain.filter(exchange);
+        };
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, String msg, HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+
+        return response.setComplete();
+    }
+
+    private boolean isJwtValid(String jwt) {
+        boolean isJwtValid = true;
+
+        String subject = null;
+
+        try {
+            subject = Jwts.parser()
+                    .setSigningKey(environment.getProperty("token.secret"))
+                    .parseClaimsJws(jwt)
+                    .getBody()
+                    .getSubject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        if (subject == null || subject.isEmpty()) {
+            isJwtValid = false;
+        }
+
+        return isJwtValid;
+    }
+}
